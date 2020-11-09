dcb2f1cb3d12fc730cc24e4a0ddedd5d8a149afc
Mid2015M32 <mid2015m32@Mid2015.local>
2020-11-09 14:32:33 +0900

    GraphQL For Chat Update

--- graphql.js
+++ graphql.js
@@ -3,28 +3,64 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const { rndStringGenerator } = require('./rndStringGenerator');
 let user = [];
+let Rooms = []
+let Chattings = [];
 var schema = buildSchema(`
   type User {
     id : ID!
     name : String!
-    friends : [ID]!
+    phone : String!
+    email : String!
+    friends : [User!]
+    rooms : [Room!]
   }
   input UserInput {
     name : String!
+    phone : String!
+    email : String!
   }
+
+  input FriendInput {
+    id : ID
+    name : String
+    phone : String
+    email : String
+  }
+
+  type Room {
+    id : ID!
+    name : String!
+    newlog : [Chat!]
+    oldlog : [Chat!]
+  }
+
+  type Chat {
+    data : String!
+  }
+
   type Query {
     user(id : ID!) : User
     users : User
   }
 
   type Mutation {
-    createUser(input : UserInput) : User
-    insertFriend(id : ID!) : User
+    addUser(input : UserInput) : User
+    addFriend(input : FriendInput) : User
   }
 `)
 
+class User {
+  constructor(props) {
+    this.id = props.id,
+      this.name = props.name,
+      this.phone = props.phone,
+      this.email = props.email,
+      this.friends = props.friends
+  }
+}
+
 const resolver = {
-  user : ({id}) => {
+  user: ({ id }) => {
     var data = Object.keys(user).filter(element => {
       if (element.id == id) {
         return element;
@@ -32,21 +68,40 @@ const resolver = {
     })
     return user[data]
   },
-  users : () => {
+
+  users: () => {
     return user
   },
-  createUser: ({ input }) => {
-    var objectID = rndStringGenerator();
-    user.push({id : objectID, name : input.name, friends : []})
-    return { id: objectID, name: input.name, friends : [] };
-  },
-  insertFriend : ({id, targetID}) => {
-    Object.keys(user).filter(element => {
-      if (element.id == id) {
 
+  addUser: ({ input }) => {
+    var objectID = rndStringGenerator();
+    var rawObject = {
+      id: objectID, name: input.name, phone: input.phone, email: input.email, friends: []
+    }
+
+    // Replace fakeDatabase to real database Function
+    user.push(rawObject);
+    return new User(rawObject);
+  },
+  addFriend: ({ input }) => {
+    console.log(input)
+    console.log(Object.keys(user))
+
+    var data = Object.keys(user).filter(element => {
+      console.log(element)
+      if (element.name == input.name) {
+        return element
       }
-    })
-  } 
+    });
+    var data = Object.keys(user).filter(element => {
+      if (element.id == id) {
+        return element
+      }
+    });
+    user[data].friends.push()
+
+    return new User()
+  }
 }
 
 var app = express();
