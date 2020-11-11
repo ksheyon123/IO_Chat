2b2628a698dd860e19163b59c2b33e949ab6a004
Kangseohyun <kshyeon123@gmail.com>
2020-11-11 12:10:25 +0900

    db connection test

--- dbConfig.js
+++ dbConfig.js
@@ -4,6 +4,7 @@ const url = "mongodb://localhost:27017";
 
 module.exports = {
     getUser : async (id) => {
+        console.log(id)
         const client = await MongoClient.connect(url, {useUnifiedTopology : true}).catch(err => { console.log(err) });
         if (!client) return;
 
@@ -16,9 +17,44 @@ module.exports = {
 
             collectionList.forEach(element => {
                 console.log(element.name);
-            })
-        } catch (err) {
+            });
 
+            // Call Database Status
+            db.stats((err, status) => {
+                if (err) throw err;
+            });
+
+            var user = await db.collection("users").findOne({});
+            console.log(user);
+
+        } catch (err) {
+            console.log(err);
+        } finally {
+            client.close();
+        }
+    },
+    addUser : async ({data}) => {
+        console.log(data);
+        const client = await MongoClient.connect(url, {useUnifiedTopology : true}).catch(err => { console.log(err) });
+        if (!client) return;
+        try {
+            const db = client.db("OIChat");
+            // Database Collection List 호출
+            db.listCollections().toArray().then(docs => {
+                console.log("Available Collections : ");
+                docs.forEach((doc) => { console.log(doc.name)});
+            });
+
+            db.stats((err, status) => {
+                if (err) throw err;
+                console.log(status);
+            });
+
+            db.collection("users").insertOne({})
+        } catch (err) {
+            console.log(err);
+        } finally {
+            client.close();
         }
     }
 }
